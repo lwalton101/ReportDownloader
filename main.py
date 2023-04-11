8a077c64300bd773327c34dbdf78c481f8c01c65
lwalton101 <waltonfamily01@outlook.com>
2023-04-11 13:47:53 +0100

    Basic Button

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import tkinter
 
+def loginButtonCallback():
+    print("hello world")
+
 root = tkinter.Tk()
 root.geometry("400x400")
 root.title("Report Downloader")
 
-
+loginButton = tkinter.Button(root, text="Login to Amazon Ads API", command=loginButtonCallback)
+loginButton.pack()
 root.mainloop()
+
+
